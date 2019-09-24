421562d79304542065b55f2a4769a85bc66fd70d
Christie Bahlai <cbahlai@kent.edu>
2019-09-24 15:11:27 -0400

    demo functions loops and conditionals

--- hyrdation_code/scratch_code.R
+++ hyrdation_code/scratch_code.R
@@ -1,1 +1,33 @@
 #This is our very first script file
+
+a<-12
+b<-13
+c<-14
+
+domath<-function(data1, data2, data3){
+  result<-data1+data2/data3
+  return(result)
+}
+
+domath(a,b,c)
+domath(b,c,a)
+
+
+condfunction<-function(data){
+  if (data>13){
+    message<-"It's warm enough"
+  }else{
+    message<-"It's too cold, get your sweater."
+  }
+  return(message)
+}
+
+
+vec<-c(a,b,c)
+vec
+
+for (i in 1:length(vec)){
+  out<-condfunction(vec[i])
+  print (vec[i])
+  print(out)
+}
