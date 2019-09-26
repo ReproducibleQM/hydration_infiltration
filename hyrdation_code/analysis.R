b5ef4e9e9bdadf8a728f53453f9b747f40d6c594
Christie Bahlai <cbahlai@kent.edu>
2019-09-26 14:57:35 -0400

    manipulate data

--- hyrdation_code/analysis.R
+++ hyrdation_code/analysis.R
@@ -5,8 +5,8 @@
 # Snowville Soil Moisture SNs:
 #   Pre-rip, Flat:
 #   SN: 20386723 Microstation
-# SN: 20385905 20 cm
-# SN: 20385906 48 cm
+# SN: 20385905 20 cm (top)
+# SN: 20385906 48 cm (bottom)
 # Pre-rip, Slope:
 #   SN: 20386722 Microstation
 # SN: 20385903 20 cm
@@ -21,9 +21,15 @@
 # SN: 20385905 10 cm, EW rip
 # SN: 20469555 10 cm, NS rip
 
+#start with pre-rip, flat data
 data1<-read.csv(file="https://raw.githubusercontent.com/ReproducibleQM/hydration_infiltration/master/Data_%20soil_moisture/20180713_FLAT_20386723.csv", 
                 header=T, skip=1)
 
 summary(data1)
 
 names(data1)<-c("Observation","Date_time", "Water_top", "Water_bottom")
+
+incline<-rep("flat", length(data1$Observation))
+rip_status<-rep("pre", length(data1$Observation))
+
+data2<-cbind(data1,incline, rip_status)
