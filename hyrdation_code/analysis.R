0f9e96be350cbe88b9339ae0c39492e5a464ee7e
katiemmanning <kmanni12@kent.edu>
2019-10-01 14:39:57 -0400

    Code added for 2nd data file (pre-ripped, slope)

--- hyrdation_code/analysis.R
+++ hyrdation_code/analysis.R
@@ -27,8 +27,10 @@ data1<-read.csv(file="https://raw.githubusercontent.com/ReproducibleQM/hydration
 
 summary(data1)
 
+#change column names
 names(data1)<-c("Observation","Date_time", "Water_top", "Water_bottom")
 
+#add incline and rip status variables
 incline<-rep("flat", length(data1$Observation))
 rip_status<-rep("pre", length(data1$Observation))
 
@@ -39,6 +41,28 @@ library(reshape2)
 
 data3<-melt(data2, id=c("Observation", "Date_time", "incline", "rip_status"))
 
+#change column names
 names(data3)[names(data3) == "variable"] <- "sensor_depth"
 names(data3)[names(data3) == "value"] <- "water_content"
 
+#next, pre-rip, slope data
+pre_slope1<-read.csv(file="https://raw.githubusercontent.com/ReproducibleQM/hydration_infiltration/master/Data_%20soil_moisture/20180713_SLOPE_20386722.csv", 
+                header=T, skip=1)
+summary(pre_slope1)
+
+#change column names
+names(pre_slope1)<-c("Observation","Date_time", "Water_top", "Water_bottom")
+
+#add incline and rip status variables
+incline<-rep("slope", length(pre_slope1$Observation))
+rip_status<-rep("pre", length(pre_slope1$Observation))
+pre_slope2<-cbind(pre_slope1,incline, rip_status)
+
+#now we need to melt the data to get it in long form
+library(reshape2)
+
+pre_slope3<-melt(pre_slope2, id=c("Observation", "Date_time", "incline", "rip_status"))
+
+#change column names
+names(pre_slope3)[names(pre_slope3) == "variable"] <- "sensor_depth"
+names(pre_slope3)[names(pre_slope3) == "value"] <- "water_content"
