a7642e2d27196ecd80805b92f73abbf36b49740e
Christie Bahlai <cbahlai@kent.edu>
2019-10-01 15:10:44 -0400

    clean it up and merge data

--- hyrdation_code/analysis.R
+++ hyrdation_code/analysis.R
@@ -58,11 +58,13 @@ incline<-rep("slope", length(pre_slope1$Observation))
 rip_status<-rep("pre", length(pre_slope1$Observation))
 pre_slope2<-cbind(pre_slope1,incline, rip_status)
 
-#now we need to melt the data to get it in long form
-library(reshape2)
-
 pre_slope3<-melt(pre_slope2, id=c("Observation", "Date_time", "incline", "rip_status"))
 
 #change column names
 names(pre_slope3)[names(pre_slope3) == "variable"] <- "sensor_depth"
 names(pre_slope3)[names(pre_slope3) == "value"] <- "water_content"
+
+#now we need to bring our data frames together
+prerip<-rbind(data3, pre_slope3)
+
+summary(prerip)
