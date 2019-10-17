445f24570b534ac1978b726a82a15a77c718bbfd
Zia <52656992+Zhassan1@users.noreply.github.com>
2019-10-16 21:51:53 -0400

    Post_flat_may changes

--- hyrdation_code/analysis.R
+++ hyrdation_code/analysis.R
@@ -135,32 +135,32 @@ postslope_oct3$sensor_depth<-ifelse(postslope_oct3$sensor=="Water_crossrip",
 
 #first attempt at pulling in SN20386722 - cross rip, EW, and NS rips
 
-post_Cross_EWNS<-read.csv(file="https://raw.githubusercontent.com/ReproducibleQM/hydration_infiltration/master/Data_%20soil_moisture/20190628_SnowvilleSlope_20386722_.csv",
+post_flat_may1<-read.csv(file="https://raw.githubusercontent.com/ReproducibleQM/hydration_infiltration/master/Data_%20soil_moisture/20190628_SnowvilleSlope_20386722_.csv",
                           header=T, skip=1)
 
 #changing column names
-names(post_Cross_EWNS)<-c("Observation","Date_Time","Cross_Rip","EW_Rip","NS_Rip")
+names(post_flat_may1)<-c("Observation","Date_Time","Cross_Rip","EW_Rip","NS_Rip")
 
-summary(post_Cross_EWNS)
+summary(post_flat_may1)
 
 #so far so good, now adding incline and rip status
-incline<-rep("slope", length(post_Cross_EWNS$Observation))
-rip_status<-rep("post", length(post_Cross_EWNS$Observation))
+incline<-rep("slope", length(post_flat_may1$Observation))
+rip_status<-rep("post", length(post_flat_may1$Observation))
 
 #binding
-post_cross_EWNS2<-cbind(post_Cross_EWNS,incline,rip_status)
+post_flat_may2<-cbind(post_flat_may1,incline,rip_status)
 
 
-post_cross_EWNS3<-melt(post_cross_EWNS2, id=c("Observation", "Date_Time", "incline", "rip_status"))
+post_flat_may3<-melt(post_flat_may2, id=c("Observation", "Date_Time", "incline", "rip_status"))
 
-summary(post_cross_EWNS3)
+summary(post_flat_may3)
 
 #still looks good - now I'll change the column names
-names(post_cross_EWNS3)[names(post_cross_EWNS3) == "variable"]<-"sensor"
-names(post_cross_EWNS3)[names(post_cross_EWNS3) == "value"]<-"water_content"
+names(post_flat_may3)[names(post_flat_may3) == "variable"]<-"sensor"
+names(post_flat_may3)[names(post_flat_may3) == "value"]<-"water_content"
 
 #now I have to add a sensor depth column and give it values
-post_cross_EWNS3$sensor_depth<-ifelse(post_cross_EWNS3$sensor=="Cross_Rip", 10,10)
+post_flat_may3$sensor_depth<-ifelse(post_flat_may3$sensor=="Cross_Rip", 10,10)
 
 #I realized later that every sensor depth for this data set is 10
 
